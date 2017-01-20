83221eb87b61034bc8ab25f34ac4f8f92783e4fe
decrek <declanrek@gmail.com>
2017-01-20 16:49:40 +0100

    01-minify-exercise

---
+++ README.md
@@ -0,0 +1,16 @@
+# Minify HTML, CSS, JS
+
+## Exercise
+
+* Change [tasks/html.js](tasks/html.js) to minify HTML.
+* Change [tasks/styles.js](tasks/styles.js) to minify CSS.
+* Change [tasks/scripts.js](tasks/scripts.js) to minify JS.
+
+## Solution
+
+See [01-minify-solution](https://github.com/voorhoede/front-end-performance-masterclass/tree/01-minify-solution).
+
+---
+
+Return to [Front-end Performance Masterclass](https://github.com/voorhoede/front-end-performance-masterclass).
+
---
+++ tasks/scripts.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify'); // docs: https://www.npmjs.com/package/gulp-uglify
+
+const baseDir = 'src/optimised/';
+
+gulp.src([
+        baseDir + '/dist/js/bootstrap.js',
+        baseDir + '/assets/js/docs.min.js',
+        baseDir + '/assets/js/ie10-viewport-bug-workaround.js'
+    ])
+    .pipe(concat('index.js'))
+    // todo: minify JS
+    //.pipe()
+    .pipe(gulp.dest('dist/optimised/'));
