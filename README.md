28dd38f7456c461a9006fc3a9dbea56afdd7fbfb
decrek <declanrek@gmail.com>
2017-01-24 11:38:16 +0100

    05-caching-exercise

---
+++ README.md
@@ -0,0 +1,14 @@
+# Set cache header
+
+## Exercise
+
+Write your own [Express middleware](https://expressjs.com/en/guide/writing-middleware.html) in [server-optimised.js](server-optimised.js#L15) to set the cache header of every file to 1 year.
+
+
+## Solution
+
+See [05-caching-solution](https://github.com/voorhoede/front-end-performance-masterclass/tree/05-caching-solution).
+
+---
+
+Return to [Front-end Performance Masterclass](https://github.com/voorhoede/front-end-performance-masterclass).
---
+++ server-optimised.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const routeStatic = require('./lib/route-static');
+const redirectIndices = require('./lib/redirect-indices');
+
+const app = express();
+const baseDir = 'dist/optimised';
+const port = process.env.PORT || 3004;
+
+app.set('etag', false);
+app.use((req, res, next) => { res.removeHeader('X-Powered-By'); next(); });
+
+app.use((req, res, next) => {
+    // todo: set cache header to 1 year
+    next();
+});
+
+// static routes
+app.use(routeStatic);
+app.use('/static', express.static(path.join(__dirname, baseDir), { etag: false, lastModified: false }));
+
+// dynamic pages
+app.use(redirectIndices);
+nunjucks.configure(baseDir, {
+    autoescape: true,
+    express: app,
+    watch: true
+});
+app.get('*', (req, res, next) => {
+    res.render(path.join('./', req.url, 'index.html'), {});
+});
+
+app.listen(port, (err) => {
+    err ? console.error(err) : console.log(`optimised app running on http://localhost:${port}`);
+});
