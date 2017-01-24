cde7c0554e05406f3b4c48bb9da7d5557b36d743
decrek <declanrek@gmail.com>
2017-01-24 11:08:37 +0100

    04-picture-exercise

---
+++ README.md
@@ -0,0 +1,15 @@
+# Use `picture` elements
+
+## Exercise
+
+Use `picture` elements for the "expo" images in [src/optimised/index.html](src/optimised/index.html) (starting with `src="/assets/img/expo-*"`) to load the optimal size and format (`jpg` or `webp`) image.
+You can find the scaled images in `jpg` and `webp` format in [src/optimised/assets/img/](src/optimised/assets/img/).
+
+
+## Solution
+
+See [04-picture-solution](https://github.com/voorhoede/front-end-performance-masterclass/tree/04-picture-solution).
+
+---
+
+Return to [Front-end Performance Masterclass](https://github.com/voorhoede/front-end-performance-masterclass).
---
+++ tasks/styles.js
@@ -0,0 +1,23 @@
+const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano'); // docs: https://www.npmjs.com/package/gulp-cssnano
+
+const baseDir = 'src/optimised/';
+
+gulp.src([
+        baseDir + '/dist/css/fonts.css',
+        baseDir + '/dist/css/bootstrap.css',
+        baseDir + '/assets/css/src/docs.css'
+    ])
+    .pipe(concat('index.css'))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
+    // minify CSS
+    .pipe(cssnano({
+        discardComments: {
+            removeAll: true
+        }
+    }))
+    .pipe(gulp.dest('dist/optimised/'));
